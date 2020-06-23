4779e4fc04a6184e3ae38324f204dfdb315127d8
Paul Gellai <paul@paul.systems>
2020-06-22 23:41:49 -0700

    update cors policy

--- router/main.go
+++ router/main.go
@@ -31,7 +31,7 @@ func (info *Router) Launch() {
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
+		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	})
 	r.Use(cors.Handler)
